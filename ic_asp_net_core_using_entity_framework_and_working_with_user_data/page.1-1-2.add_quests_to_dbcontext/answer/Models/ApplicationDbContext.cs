8b3d66ea88722a0dc9a82bc377a991fe37a5477c
snychka <stefannychka@gmail.com>
2021-10-19 18:07:30 -0600

    fix so it works when run

--- ic_asp_net_core_using_entity_framework_and_working_with_user_data/page.1-1-2.add_quests_to_dbcontext/answer/Models/ApplicationDbContext.cs
+++ ic_asp_net_core_using_entity_framework_and_working_with_user_data/page.1-1-2.add_quests_to_dbcontext/answer/Models/ApplicationDbContext.cs
@@ -1,12 +1,24 @@
-using System;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.InMemory;
 
 namespace ForgingAhead.Models
 {
-  public class ApplicationDbContext : DbContext
-  {
-    public DbSet<Character> Characters { get; set; }
-    public DbSet<Equipment> Equipment { get; set; }
-    public DbSet<Quest> Quests { get; set; }
-  }
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Character> Characters { get; set; }
+        public DbSet<Equipment> Equipment { get; set; }
+        public DbSet<Quest> Quests { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            options.UseInMemoryDatabase("ForgingAhead");
+            base.OnConfiguring(options);
+        }
+    }
 }
+
