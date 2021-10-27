526c9e4ca905fcb63ff0e8c1dac1382bac9f6458
Stefan Nychka <stefannychka@gmail.com>
2021-10-27 11:22:58 -0600

    allow for in-ch browser

--- ic_phoenix_creating_a_basic_web_app/page.2-1-3.listing_events/answer/lib/neon_web/router.ex
+++ ic_phoenix_creating_a_basic_web_app/page.2-1-3.listing_events/answer/lib/neon_web/router.ex
@@ -5,8 +5,8 @@ defmodule NeonWeb.Router do
     plug :accepts, ["html"]
     plug :fetch_session
     plug :fetch_flash
-    plug :protect_from_forgery
-    plug :put_secure_browser_headers
+    # plug :protect_from_forgery
+    # plug :put_secure_browser_headers
   end
 
   pipeline :api do
