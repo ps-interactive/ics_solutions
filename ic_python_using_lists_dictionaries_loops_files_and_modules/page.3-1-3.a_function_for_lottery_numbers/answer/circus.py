6492f168128b0c0703638beec55c4bfd448a60f9
snychka <stefannychka@gmail.com>
2021-10-29 08:15:14 -0600

    allow for output

--- ic_python_using_lists_dictionaries_loops_files_and_modules/page.3-1-3.a_function_for_lottery_numbers/answer/circus.py
+++ ic_python_using_lists_dictionaries_loops_files_and_modules/page.3-1-3.a_function_for_lottery_numbers/answer/circus.py
@@ -1,9 +1,11 @@
 import random
 
-
 def lotto_numbers():
+    # code in the function goes here
     lotto_nums = []
     for i in range(5):
         lotto_nums.append(random.randint(1, 53))
 
     return lotto_nums
+
+print(lotto_numbers())
